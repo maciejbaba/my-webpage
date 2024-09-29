ceccfdf9516f4185d3587d6f434fa46986eb004a
Maciej Baba <maciek2baba2@gmail.com>
2024-09-29 21:50:46 +0200

    Revert "add live preview of a cms project"
    This reverts commit 413f54209834b19678958170d209d65904133151.

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
 const yearSpan = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
-
-const darkModeBtn = document.getElementById("dark-mode-toggle");
-darkModeBtn.addEventListener("click", toggleDarkMode);
-
-function toggleDarkMode() {
-  console.log("test");
-}
