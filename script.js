413f54209834b19678958170d209d65904133151
Maciej Baba <maciek2baba2@gmail.com>
2024-09-29 21:49:22 +0200

    add live preview of a cms project

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const yearSpan = document.getElementById("year");
 const currentYear = new Date().getFullYear();
 yearSpan.textContent = currentYear;
+
+const darkModeBtn = document.getElementById("dark-mode-toggle");
+darkModeBtn.addEventListener("click", toggleDarkMode);
+
+function toggleDarkMode() {
+  console.log("test");
+}
